d88b40d5c8fc2a51e747cc283b26bd7a85c47816
Hyeonjin Kim <andrew75313@gmail.com>
2024-06-14 11:20:08 +0900

    test(dto): FeedReqDto 단위 테스트
    -생성자 테스트
    -필드 Validation 테스트

---
+++ src/test/java/com/sparta/newsfeedteamproject/dtoTest/FeedReqDtoTest.java
@@ -0,0 +1,63 @@
+package com.sparta.newsfeedteamproject.dtoTest;
+
+import com.sparta.newsfeedteamproject.dto.feed.FeedReqDto;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FeedReqDtoTest {
+
+    Validator validator;
+    FeedReqDto feedReqDto;
+
+    @BeforeEach
+    void setUp() {
+        feedReqDto = new FeedReqDto();
+
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+
+    // 정상적인 유효한 FeedReqDto 생성 테스트
+    @Test
+    @DisplayName("유효한 FeedReqDto 생성 테스트")
+    void testValidContents() throws NoSuchFieldException, IllegalAccessException {
+        // given
+        String contents = "Feed test";
+
+        // when
+        Field field = FeedReqDto.class.getDeclaredField("contents");
+        field.setAccessible(true);
+        field.set(feedReqDto, contents);
+
+        // then
+        assertEquals(contents, feedReqDto.getContents(), "Contents가 올바르게 설정되지 않았습니다.");
+    }
+
+    // Contents 필드 유효성 테스트
+    @Test
+    @DisplayName("Contents 필드 @Notblank Validation 테스트")
+    void testContentsNotBlank() throws NoSuchFieldException, IllegalAccessException {
+        // given
+        String contents = "";
+
+        Field field = FeedReqDto.class.getDeclaredField("contents");
+        field.setAccessible(true);
+        field.set(feedReqDto, contents);
+
+        // when
+        ConstraintViolation<FeedReqDto> violation = validator.validate(feedReqDto).stream().collect(Collectors.toList()).get(0);
+
+        // then
+        assertEquals("contents", violation.getPropertyPath().toString(), "@NotBlank Validation 적용이 안되었습니다.");
+    }
+}
