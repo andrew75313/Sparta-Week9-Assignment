67ae8bae451272f04f3d07a8d4089ff9f5f2d315
Hyeonjin Kim <andrew75313@gmail.com>
2024-06-17 17:07:10 +0900

    fix: (feedback) AOP 기존 로깅 사항 추가
    -log 문구 변경
    -parameter도 로깅되게 추가
    -실행되는 메서드 이름도 로깅되게 추가
    -실행되는 메서드 정보를 가져올 수 있도록 매개변수 설정

--- src/main/java/com/sparta/newsfeedteamproject/aop/RequestLogAop.java
+++ src/main/java/com/sparta/newsfeedteamproject/aop/RequestLogAop.java
@@ -3,6 +3,7 @@ package com.sparta.newsfeedteamproject.aop;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -10,6 +11,9 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 // 모든 API(Controller)가 호출될 때, Request 정보(Request URL, HTTP Method)를
 // @Slf4J Logback 라이브러리를  활용하여 Log로 출력
@@ -21,11 +25,12 @@ public class RequestLogAop {
 
     // 모든 Controller의 모든 메서드 Pointcut 지정
     @Pointcut("execution(* com.sparta.newsfeedteamproject.controller.*.*(..))")
-    private void controller() {}
+    private void controller() {
+    }
 
     // Controller 실행 전후에 Request 정보 Log로 출력 = void
     @Around("controller()")
-    public void logRequestInfo() {
+    public Object logRequestInfo(ProceedingJoinPoint joinPoint) throws Throwable {
 
         // 요청 ContextHolder에서 서블릿 객체 속성 캐스팅해 구현체로 가지고 오기
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
@@ -33,17 +38,32 @@ public class RequestLogAop {
         // 요청 null check
         if (attributes == null) {
             log.warn("Request 정보가 없습니다."); // info -> warn
+            return joinPoint.proceed();
         }
 
         // 서블릿 객체의 속성 중 HttpServletRequest 요청 정보 가지고 오기
         HttpServletRequest request = attributes.getRequest();
 
         // Request URL log
-        String url = request.getRequestURL().toString();
-        log.info("Request URL : " + url);
+        String uri = request.getRequestURL().toString();
 
-        // Request Mothod log
+        // Request Method log
         String method = request.getMethod().toString();
-        log.info("Request HTTP Method : " + method);
+
+        // Request parameter log
+        String params = request.getParameterMap().entrySet().stream()
+                .map(entry -> entry.getKey() + " : " + String.join(",", entry.getValue()))
+                .collect(Collectors.joining(" | "));
+
+        // Controller Method Name log
+        String controllerMethodName = joinPoint.getSignature().getName();
+
+
+        log.info("Request HTTP Method : {}", method);
+        log.info("Request URI : {}", uri);
+        log.info("Request Parameter : {}", params);
+        log.info("Controller Method : {}", controllerMethodName);
+
+        return joinPoint.proceed();
     }
 }
